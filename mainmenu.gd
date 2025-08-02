19c3e973466aeef32c7daebe58d3dd6dd7fa8bbe
HectorStratford <217393740+hectorstratford@users.noreply.github.com>
2025-08-02 18:04:46 -0500

    Add basic enemy

--- mainmenu.gd
+++ mainmenu.gd
@@ -9,11 +9,10 @@ func _ready():
 	start_button.pressed.connect(_on_start_button_pressed)
 	settings_button.pressed.connect(_on_settings_button_pressed)
 	credits_button.pressed.connect(_on_credits_button_pressed)
-	exit_button.pressed.connect(_on_exit_button_pressed)
 
 func _on_start_button_pressed():
 	print("Start Game pressed")
-	# TODO: Implement game start logic
+	get_tree().change_scene_to_file("res://player.tscn")
 
 func _on_settings_button_pressed():
 	print("Settings pressed")
@@ -22,7 +21,3 @@ func _on_settings_button_pressed():
 func _on_credits_button_pressed():
 	print("Credits pressed")
 	# TODO: Implement credits screen
-
-func _on_exit_button_pressed():
-	print("Exit Game pressed")
-	get_tree().quit()
--- player.gd
+++ player.gd
@@ -1,7 +1,13 @@
 extends Area2D
+signal hit
 
 @export var speed = 400
 var screen_size
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
 
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -24,3 +30,18 @@ func _process(delta):
 		$AnimatedSprite2D.stop()
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "default"
+		$AnimatedSprite2D.flip_v = false
+		# See the note below about the following boolean assignment.
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "default"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+
+func _on_body_entered(_body):
+	hide() # Player disappears after being hit.
+	hit.emit()
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true)
